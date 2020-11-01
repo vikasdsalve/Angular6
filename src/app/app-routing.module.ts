c54e911c375ec4ced41d7939d55af5e86f3d7a04
Vikas Salve <vikas.salve@capgemini.com>
2020-11-01 22:33:18 +0530

    Part 3 Angular 6 routing tutorial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListEmployeesComponent } from './employee/list-employees.component';
+import { CreateEmployeeComponent } from './employee/create-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'list', component: ListEmployeesComponent},
+{path: 'create', component: CreateEmployeeComponent},
+{path: '', redirectTo: '/list', pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './employee/create-employee.component';
+import { ListEmployeesComponent } from './employee/list-employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    ListEmployeesComponent
   ],
   imports: [
     BrowserModule,
