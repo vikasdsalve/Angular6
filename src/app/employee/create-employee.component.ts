1fe14f27e622079320947a271e6f7bda033d5cd8
Vikas Salve <vikas.salve@capgemini.com>
2020-11-02 06:16:49 +0530

    Part 10 Angular form control valuechanges

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -2,6 +2,7 @@ import { variable } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
@@ -9,6 +10,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class CreateEmployeeComponent implements OnInit {
   employeeForm: FormGroup;
+  fullNameLength = 0;
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
@@ -21,26 +23,26 @@ export class CreateEmployeeComponent implements OnInit {
         proficiency: ['beginner']
       })
     });
-  }
+}
 
-  onLoadDataClick(): void {
-    this.employeeForm.setValue({
-      fullName: 'Vikas Salve',
-      email: 'Vikas.dsalve@hotmail.com',
-      skills: {
-        skillName: 'C#',
-        experienceInYears: 5,
-        proficiency: 'beginner'
-      }
-    });
-  }
+onLoadDataClick(): void {
+  this.employeeForm.setValue({
+    fullName: 'Vikas Salve',
+    email: 'Vikas.dsalve@hotmail.com',
+    skills: {
+      skillName: 'C#',
+      experienceInYears: 5,
+      proficiency: 'beginner'
+    }
+  });
+}
 
-  onSubmit(): void {
-    console.log(this.employeeForm.touched);
-    console.log(this.employeeForm.value);
+onSubmit(): void {
+  console.log(this.employeeForm.touched);
+  console.log(this.employeeForm.value);
 
-    console.log(this.employeeForm.controls.fullName.touched);
-    console.log(this.employeeForm.get('fullName').value);
-  }
+  console.log(this.employeeForm.controls.fullName.touched);
+  console.log(this.employeeForm.get('fullName').value);
+}
 
 }
