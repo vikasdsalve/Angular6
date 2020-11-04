4e8c8925a98c4471a9810a43b502661f63c68d88
Vikas Salve <vikas.salve@capgemini.com>
2020-11-04 11:04:54 +0530

    Part 25 Remove dynamically created form controls in angular

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -73,6 +73,9 @@ export class CreateEmployeeComponent implements OnInit {
     (<FormArray>this.employeeForm.get('skills')).push(this.addSkillFormGroup());
   }
 
+  removeSkillButtonClick(skillGroupIndex: number): void {
+    (<FormArray>this.employeeForm.get('skills')).removeAt(skillGroupIndex);
+  }
     addSkillFormGroup(): FormGroup{
       return this.fb.group({
         skillName: ['', Validators.required],
