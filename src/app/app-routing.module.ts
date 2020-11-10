a6349ca4b19403adf81800d70d575d20b90fea37
Vikas Salve <vikas.salve@capgemini.com>
2020-11-10 18:44:32 +0530

    Part 36 Lazy loading in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ import { PageNotFoundComponent } from './page-not-found.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'employees',
+    loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule)
+  },
   { path: '**', component: PageNotFoundComponent }
 
 ];
--- src/app/employee/employee-routing.module.ts
+++ src/app/employee/employee-routing.module.ts
@@ -5,11 +5,11 @@ import { CreateEmployeeComponent } from './create-employee.component';
 
 
 const routes: Routes = [
-  { path: 'employees', children: [
+
     { path: '', component: ListEmployeesComponent },
     { path: 'create', component: CreateEmployeeComponent },
     { path: 'edit/:id', component: CreateEmployeeComponent }
-  ] }
+  
 ];
 
 @NgModule({
